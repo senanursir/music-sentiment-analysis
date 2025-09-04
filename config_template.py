384d4f7fc24aca7fb15784be70e4cc429705d6db
Sena Sır <sirsena10@gmail.com>
2025-09-04 15:41:02 +0300

    add config template for API keys

---
+++ config_template.py
@@ -0,0 +1,4 @@
+# You can add your own secret login credentials here.
+SPOTIFY_CLIENT_ID = "your_spotify_client_id_here"
+SPOTIFY_CLIENT_SECRET = "your_spotify_client_secret_here"
+GENIUS_ACCESS_TOKEN = "your_genius_access_token_here"
